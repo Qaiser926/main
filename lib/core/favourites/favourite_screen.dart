34911fdc6ee65751daf57bd59e5f55230060893e
Hannes Kunstmann <h.kunstmann@outlook.com>
2022-11-30 09:36:26 +0100

    try and fail mode

--- lib/core/favourites/favourite_screen.dart
+++ lib/core/favourites/favourite_screen.dart
@@ -5,6 +5,7 @@ import 'package:othia/constants/asset_constants.dart';
 
 import '../../config/themes/color_data.dart';
 import '../../utils/services/from_bought_ui/widget_utils.dart';
+import '../../utils/ui/event_section.dart';
 import '../../utils/ui/upcoming_past_eventlist.dart';
 import '../../widgets/event_activity_list.dart';
 
@@ -33,6 +34,7 @@ class _FavouritePageState extends State<FavouritePage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: buildAppBar(),
       body: NestedScrollView(
         controller: _scrollController,
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
@@ -42,10 +44,16 @@ class _FavouritePageState extends State<FavouritePage>
 
 
             SliverAppBar(
+              toolbarHeight: 50.h,
               backgroundColor: Colors.white,
+              elevation: 0,
+              centerTitle: true,
 
-              title: getCustomFont("Favoriten", 24.sp, Colors.black, 1,
-                  fontWeight: FontWeight.w700, textAlign: TextAlign.center),
+
+
+              title: Row(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min, children: [getCustomFont("Favoriten", 24.sp, Colors.black, 1,
+                    fontWeight: FontWeight.w700, textAlign: TextAlign.center),],),
                 automaticallyImplyLeading: false,
               pinned: true,
               floating: false,
@@ -53,8 +61,12 @@ class _FavouritePageState extends State<FavouritePage>
 
               forceElevated: innerBoxIsScrolled,
               bottom: AppBar(
+                toolbarHeight: 100.h,
+
+                elevation: 0,
                 backgroundColor: Colors.white,
                   automaticallyImplyLeading: false,
+          // TODO try container in container approach but first try to implement that headings are really exchanged
           title: Container(
       // space between blue and and white
       height: 50,
@@ -67,46 +79,46 @@ class _FavouritePageState extends State<FavouritePage>
             BoxShadow(
                 color: shadowColor, offset: const Offset(0, 8), blurRadius: 27)
           ]),
-      child:
-              TabBar(
+      child: TabBar(
 
 
-                  controller: _tabController,
-                  unselectedLabelColor: greyColor,
-                  labelColor: Colors.white,
-                  indicatorSize: TabBarIndicatorSize.tab,
-                  indicator: BoxDecoration(
-                      borderRadius: BorderRadius.circular(44.h), color: accentColor),
-                  onTap: (index) {
-                    pageController.animateToPage(
-                      index,
-                      duration: const Duration(milliseconds: 400),
-                      curve: Curves.easeInOut,
-                    );
-                  },
-                  tabs: [
-                    Tab(
-                      child: Align(
-                        alignment: Alignment.center,
-                        // todo language setting
-                        child: Text("Events",
-                            style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontFamily: AssetConstants.fontsFamily,
-                                fontSize: 18.sp)),
-                      ),
-                    ),
-                    Tab(
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Text("Aktivitäten",
-                            style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontFamily: AssetConstants.fontsFamily,
-                                fontSize: 18.sp)),
-                      ),
-                    ),
-                  ]),
+          controller: _tabController,
+          unselectedLabelColor: greyColor,
+          labelColor: Colors.white,
+          indicatorSize: TabBarIndicatorSize.tab,
+          indicator: BoxDecoration(
+              borderRadius: BorderRadius.circular(44.h), color: accentColor),
+          onTap: (index) {
+            pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 400),
+              curve: Curves.easeInOut,
+            );
+          },
+          tabs: [
+            Tab(
+              child: Align(
+                alignment: Alignment.center,
+                // todo language setting
+                child: Text("Events",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontFamily: AssetConstants.fontsFamily,
+                        fontSize: 18.sp)),
+              ),
+            ),
+            Tab(
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Aktivitäten",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontFamily: AssetConstants.fontsFamily,
+                        fontSize: 18.sp)),
+              ),
+            ),
+          ])
+              ,
             ))),
           ];
         },
@@ -122,17 +134,31 @@ class _FavouritePageState extends State<FavouritePage>
   }
 
   _pageView() {
-    return ListView.builder(
-      itemCount: 20,
-      itemBuilder: (BuildContext context, int index) {
-        return Card(
-          child: Container(
-            padding: EdgeInsets.all(16.0),
-            child: Text('List Item $index'),
-          ),
-        );
-      },
+    return CustomScrollView(
+      slivers: [
+        Section(
+          title: 'Category #1',
+          headerColor: Colors.blue,
+          items: List.generate(10, (index) => ListTile(
+            title: Text('Item #${index + 1}'),
+          )),
+        ),
+        Section(
+          title: 'Category #2',
+          headerColor: Colors.red,
+          items: List.generate(10, (index) => ListTile(
+            title: Text('Item #${index + 11}'),
+          )),
+        ),
+      ]
     );
+  }
+   AppBar buildAppBar() {
+    return getToolBar(() {},
+        // TODO align for different languages
+        title: getCustomFont("Favoriten", 24.sp, Colors.black, 1,
+            fontWeight: FontWeight.w700, textAlign: TextAlign.center),
+        leading: false);
   }
 
 }
