f334c3f1e8990099f0589fd09c6d4d28c8c31330
Mattis Kunstmann <mattis.kunstmann@outlook.de>
2022-12-06 17:21:38 +0100

    fixes after merge

---
+++ lib/modules/models/shared_data_models.g.dart
@@ -0,0 +1,51 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shared_data_models.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Location _$LocationFromJson(Map<String, dynamic> json) => Location(
+      isOnline: json['isOnline'] as bool,
+      locationId: json['locationId'] as String?,
+      streetNumber: json['streetNumber'] as String?,
+      street: json['street'] as String?,
+      city: json['city'] as String?,
+      locationTitle: json['locationTitle'] as String?,
+      latitude: (json['latitude'] as num?)?.toDouble(),
+      longitude: (json['longitude'] as num?)?.toDouble(),
+    );
+
+Map<String, dynamic> _$LocationToJson(Location instance) => <String, dynamic>{
+      'isOnline': instance.isOnline,
+      'streetNumber': instance.streetNumber,
+      'street': instance.street,
+      'city': instance.city,
+      'locationTitle': instance.locationTitle,
+      'locationId': instance.locationId,
+      'longitude': instance.longitude,
+      'latitude': instance.latitude,
+    };
+
+Time _$TimeFromJson(Map<String, dynamic> json) => Time(
+      startTimeUtc: json['startTimeUtc'] as String?,
+      openingTimeCode: $enumDecodeNullable(
+          _$OpeningTimeCodeEnumMap, json['openingTimeCode']),
+      endTimeUtc: json['endTimeUtc'] as String?,
+      openingTime: json['openingTime'] as Map<String, dynamic>?,
+    );
+
+Map<String, dynamic> _$TimeToJson(Time instance) => <String, dynamic>{
+      'startTimeUtc': instance.startTimeUtc,
+      'endTimeUtc': instance.endTimeUtc,
+      'openingTimeCode': _$OpeningTimeCodeEnumMap[instance.openingTimeCode],
+      'openingTime': instance.openingTime,
+    };
+
+const _$OpeningTimeCodeEnumMap = {
+  OpeningTimeCode.open: 'open',
+  OpeningTimeCode.closed: 'closed',
+  OpeningTimeCode.openSoon: 'openSoon',
+  OpeningTimeCode.closedSoon: 'closedSoon',
+};
