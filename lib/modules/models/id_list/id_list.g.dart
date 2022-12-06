8d5905a2b53ebacaa1d396591c00962d8d8abb85
Hannes Kunstmann <h.kunstmann@outlook.com>
2022-12-06 23:10:50 +0100

    working version

---
+++ lib/modules/models/id_list/id_list.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'id_list.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+IdList _$IdListFromJson(Map<String, dynamic> json) => IdList(
+      eaIdList: json['eaIdList'] as List<dynamic>,
+    );
+
+Map<String, dynamic> _$IdListToJson(IdList instance) => <String, dynamic>{
+      'eaIdList': instance.eaIdList,
+    };
