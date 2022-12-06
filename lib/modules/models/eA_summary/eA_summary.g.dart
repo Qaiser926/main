97abf62740ed26d1bb783efeba9408031757aff1
Hannes Kunstmann <h.kunstmann@outlook.com>
2022-12-06 17:30:03 +0100

    safety copy

---
+++ lib/modules/models/eA_summary/eA_summary.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'eA_summary.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SummaryEventOrActivity _$SummaryEventOrActivityFromJson(
+        Map<String, dynamic> json) =>
+    SummaryEventOrActivity(
+      time: Time.fromJson(json['time'] as Map<String, dynamic>),
+      location: Location.fromJson(json['location'] as Map<String, dynamic>),
+      title: json['title'] as String,
+      id: json['id'] as String,
+      categoryId: json['categoryId'] as String,
+      prices: (json['prices'] as List<dynamic>?)
+          ?.map((e) => (e as num).toDouble())
+          .toList(),
+      photo: json['photo'] as String?,
+      isOnline: json['isOnline'] as bool,
+    );
+
+Map<String, dynamic> _$SummaryEventOrActivityToJson(
+        SummaryEventOrActivity instance) =>
+    <String, dynamic>{
+      'title': instance.title,
+      'id': instance.id,
+      'categoryId': instance.categoryId,
+      'photo': instance.photo,
+      'prices': instance.prices,
+      'isOnline': instance.isOnline,
+      'time': instance.time,
+      'location': instance.location,
+    };
