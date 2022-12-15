d6dc1de1f0d26021cc84ccfb054d66483a96b365
Hannes Kunstmann <h.kunstmann@outlook.com>
2022-12-15 11:29:41 +0100

    search results statically but working

---
+++ lib/core/detailed/exclusive_widgets/horizontal_exploration_detail.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:othia/utils/services/exceptions.dart';
+
+import '../../../constants/categories.dart';
+import '../../../modules/models/id_list/id_list.dart';
+import '../../../utils/services/data_handling/keep_alive_future_builder.dart';
+import '../../../utils/services/rest-api/rest_api_service.dart';
+import '../../../utils/ui/future_service.dart';
+import '../../../widgets/disccover_horizontally.dart';
+
+enum ListFunction {
+  getEAIdsForCategory,
+  getEAIdsForEventSeries,
+  getEAIdsForLocation,
+}
+
+Future<Object> getFutureList(
+    {required ListFunction listFunction, required String functionParameter}) {
+  if (listFunction.index == 0) {
+    return RestService().getEAIdsForCategory(categoryId: functionParameter);
+  }
+  if (listFunction.index == 0) {
+    return RestService()
+        .getEAIdsForEventSeries(eventSeriesId: functionParameter);
+  } else {
+    return RestService().getEAIdsForLocation(locationId: functionParameter);
+  }
+}
+
+class ExploreLocation extends StatelessWidget {
+  final String locationId;
+
+  const ExploreLocation({super.key, required this.locationId});
+
+  @override
+  Widget build(BuildContext context) {
+    late Future<Object> eAIds = getFutureList(
+        functionParameter: locationId,
+        listFunction: ListFunction.getEAIdsForLocation);
+    return getHorizontalDiscovery(
+        eAIds: eAIds, heading: AppLocalizations.of(context)!.exploreLocation);
+  }
+}
+
+class ExploreCategory extends StatelessWidget {
+  final String categoryId;
+  late String categoryName;
+
+  ExploreCategory({super.key, required this.categoryId});
+
+  @override
+  Widget build(BuildContext context) {
+    categoryName = CategoryIdToI18nMapper.fckMethod(context, categoryId);
+    late Future<Object> eAIds = getFutureList(
+        functionParameter: categoryId,
+        listFunction: ListFunction.getEAIdsForCategory);
+    return getHorizontalDiscovery(
+        eAIds: eAIds,
+        heading: AppLocalizations.of(context)!.exploreCategory(categoryName));
+  }
+}
+
+class ExploreEventSeries extends StatelessWidget {
+  final String eventSeriesId;
+
+  const ExploreEventSeries({super.key, required this.eventSeriesId});
+
+  @override
+  Widget build(BuildContext context) {
+    late Future<Object> eAIds = getFutureList(
+        functionParameter: eventSeriesId,
+        listFunction: ListFunction.getEAIdsForEventSeries);
+    return getHorizontalDiscovery(
+        eAIds: eAIds,
+        heading: AppLocalizations.of(context)!.exploreEventSeries);
+  }
+}
+
+Widget getHorizontalDiscovery(
+    {required Future eAIds, required String heading}) {
+  return KeepAliveFutureBuilder(
+      future: eAIds,
+      builder: (context, snapshot) {
+        try {
+          Map<String, dynamic> jsonData = snapshotHandler(snapshot);
+          IdList eAIds = IdList.fromJson(jsonData);
+          if (eAIds.eaIdList.length == 0) {
+            throw EmptyList;
+          }
+          return BaseDiscoveryClass(
+            heading: heading,
+            Ids: eAIds.eaIdList,
+            showMore: false,
+          );
+        } on StillLoading {
+          return SizedBox();
+        } on EmptyList {
+          return SizedBox();
+        }
+      });
+}
