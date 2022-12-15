14cff11eec20125c39a0a0dcdf4f2512741888df
Mattis Kunstmann <mattis.kunstmann@outlook.de>
2022-12-15 18:41:56 +0100

    in progress

---
+++ lib/core/add/add_exclusives/add_first_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FirstAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
+
+class SecondAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+}
