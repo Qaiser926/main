c696a83a2a7ac89bd5309b228d875d9db994046c
Hannes Kunstmann <h.kunstmann@outlook.com>
2022-12-15 19:17:45 +0100

    pictures resized. routing problems

---
+++ lib/modules/models/get_search_results_ids/get_search_result_ids.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_search_result_ids.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SearchResultIds _$SearchResultsIdsFromJson(Map<String, dynamic> json) =>
+    SearchResultIds(
+      searchResultIds: (json['searchResultIds'] as Map<String, dynamic>).map(
+        (k, e) =>
+            MapEntry(k, (e as List<dynamic>).map((e) => e as String?).toList()),
+      ),
+    );
+
+Map<String, dynamic> _$SearchResultsIdsToJson(SearchResultIds instance) =>
+    <String, dynamic>{
+      'searchResultIds': instance.searchResultIds,
+    };
