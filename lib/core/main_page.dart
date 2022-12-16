6f4c1020a3373bbe14499f6b0e88575bcf8e816d
Hannes Kunstmann <h.kunstmann@outlook.com>
2022-12-16 11:27:52 +0100

    started working on profile screen

--- lib/core/main_page.dart
+++ lib/core/main_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:othia/core/search/search.dart';
-import 'package:othia/core/settings/settings.dart';
+// import 'package:othia/core/settings/settings.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/nav_bar/nav_bar.dart';
@@ -17,7 +17,7 @@ class MainPage extends StatelessWidget {
     SearchPage(),
     const Add(),
     const FavouritePage(),
-    const SettingsPage(),
+    // const ProfilePage(),
   ];
   final PageController _pageController = PageController(initialPage: 0);
 
