f1a83dd2fd39cbdaf2f4e952e0c91e4d9f30184d
Hannes Kunstmann <h.kunstmann@outlook.com>
2022-12-16 16:37:31 +0100

    funktinierender Stand

--- lib/core/search/search_pages/initial_search_page.dart
+++ lib/core/search/search_pages/initial_search_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:othia/constants/categories.dart';
+import 'package:othia/widgets/category_filter/category_filter.dart';
 import 'package:othia/widgets/filter_related/dropdown_appbar.dart';
 import 'package:othia/widgets/filter_related/search_notifier.dart';
 import 'package:provider/provider.dart';
 
-art';
-import '../../../widgets/category_filter/category_filter.dart';
 
 class InitialSearchPage extends StatelessWidget {
   const InitialSearchPage({Key? key}) : super(key: key);
