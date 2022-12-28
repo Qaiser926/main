3d994262f84f6a52bbcd4e8249271580eb77cd13
Hannes Kunstmann <h.kunstmann@outlook.com>
2022-12-28 12:21:05 +0100

    made some changes regarding color of map cluster. Starting to change filter options

--- lib/core/map/map_results.dart
+++ lib/core/map/map_results.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_map/plugin_api.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:latlong2/latlong.dart' as latLng;
@@ -10,6 +13,7 @@ import 'package:othia/core/map/exclusive_widgets/app_bar_creator.dart';
 import 'package:othia/modules/models/get_map_result_ids/get_map_result_ids.dart';
 import 'package:othia/utils/services/rest-api/rest_api_service.dart';
 import 'package:othia/utils/ui/future_service.dart';
+import 'package:othia/utils/ui/ui_utils.dart';
 import 'package:othia/widgets/filter_related/notifiers/map_notifier.dart';
 import 'package:provider/provider.dart';
 
@@ -81,26 +85,77 @@ class _MapResultsState extends State<MapResults> {
   Widget futureMap(Map<String, dynamic> json) {
     mapResultIds = MapResultIds.fromJson(json);
     return FlutterMap(
-        options: MapOptions(
-          center: userPosition,
-          zoom: 15.0,
-          maxZoom: 17,
-          minZoom: 3,
+      options: MapOptions(
+        center: userPosition,
+        zoom: 15.0,
+        maxZoom: 18.49,
+        minZoom: 3,
+      ),
+      nonRotatedChildren: [
+        Container(
+          alignment: Alignment.bottomRight,
+          padding: const EdgeInsetsDirectional.only(end: 8, bottom: 2),
+          child: Text('© OpenStreetMap'),
         ),
-        nonRotatedChildren: [
-          Container(
-            alignment: Alignment.bottomRight,
-            padding: const EdgeInsetsDirectional.only(end: 8, bottom: 2),
-            child: Text('© OpenStreetMap'),
-          )
-        ],
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'ohtia.de',
+        // TODO align colors of Event and Activity colored box & introduce background color for the legend or change
+        // font color such that it is readible
+        Container(
+          alignment: Alignment.bottomLeft,
+          padding: const EdgeInsetsDirectional.only(start: 8, bottom: 2),
+          child: Row(
+            children: [
+              SizedBox(
+                width: 10,
+                height: 8,
+                child: DecoratedBox(
+                  decoration:
+                      BoxDecoration(color: Theme.of(context).primaryColor),
+                ),
+              ),
+              getHorSpace(5.h),
+              Text(AppLocalizations.of(context)!.event),
+              getHorSpace(10.h),
+              SizedBox(
+                width: 10,
+                height: 8,
+                child: DecoratedBox(
+                  decoration:
+                      BoxDecoration(color: Theme.of(context).bottomAppBarColor),
+                ),
+              ),
+              getHorSpace(5.h),
+              Text(AppLocalizations.of(context)!.activity),
+            ],
           ),
-          MarkerLayer(markers: getResultMarkers())
-        ]);
+        )
+      ],
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'ohtia.de',
+        ),
+        MarkerClusterLayerWidget(
+          options: MarkerClusterLayerOptions(
+            maxClusterRadius: 120,
+            size: Size(40, 40),
+            fitBoundsOptions: FitBoundsOptions(
+              padding: EdgeInsets.all(50),
+            ),
+            markers: getResultMarkers(),
+            polygonOptions:
+                PolygonOptions(color: Colors.black12, borderStrokeWidth: 3),
+            builder: (context, markers) {
+              return FloatingActionButton(
+                foregroundColor: Colors.white,
+                child: Text(markers.length.toString()),
+                onPressed: null,
+              );
+            },
+          ),
+        ),
+        // MarkerLayer(markers: getResultMarkers()),
+      ],
+    );
   }
 
   Marker getMarker(
@@ -110,7 +165,6 @@ class _MapResultsState extends State<MapResults> {
         width: 50.0,
         height: 50.0,
         rotate: true,
-        // TODO user navigator constants
         point: latLng.LatLng(locationData["coordinates"]["latitude"],
             locationData["coordinates"]["longitude"]),
         builder: (ctx) => GestureDetector(
@@ -143,12 +197,13 @@ class _MapResultsState extends State<MapResults> {
     // for activities
     //TODO align color scheme
     mapResultIds.activityResults.forEach((element) {
-      markerList
-          .add(getMarker(locationData: element!, markerColor: Colors.green));
+      markerList.add(getMarker(
+          locationData: element!, markerColor: Theme.of(context).primaryColor));
     });
     mapResultIds.eventResults.forEach((element) {
-      markerList
-          .add(getMarker(locationData: element!, markerColor: Colors.red));
+      markerList.add(getMarker(
+          locationData: element!,
+          markerColor: Theme.of(context).bottomAppBarColor));
     });
     return markerList;
   }
