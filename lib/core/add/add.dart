23678c459fdc3ac8e33f6e49bcdadd4b9866dcf3
Hannes Kunstmann <h.kunstmann@outlook.com>
2023-01-02 20:53:59 +0100

    made progress with navigation bar logic

--- lib/core/add/add.dart
+++ lib/core/add/add.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
 import 'add_exclusives/add_first_page.dart';
@@ -20,29 +21,92 @@ class Add extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    _pageController.addListener(() {
-      print(_pageController.page);
-    });
-    AddPageNotifier notifier = AddPageNotifier(firstPage);
+    AddPageNotifier switchPagesNotifier = AddPageNotifier(firstPage);
 
     AddEANotifier inputNotifier = AddEANotifier();
     return MultiProvider(
       providers: [
         ChangeNotifierProvider.value(
-          value: notifier,
+          value: switchPagesNotifier,
         ),
         ChangeNotifierProvider.value(
           value: inputNotifier,
         )
       ],
       child: Scaffold(
+        appBar: AppBar(automaticallyImplyLeading: false, actions: [
+          Consumer<AddPageNotifier>(builder: (context, switchPageModel, child) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                    padding: EdgeInsets.all(10.h),
+                    child: GestureDetector(
+                      onTap: () => {
+                        switchPagesNotifier.currentPage = 0,
+                        _pageController.jumpToPage(0)
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: switchPageModel.currentPage == 0
+                                ? Theme.of(context).colorScheme.primary
+                                : null,
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                                color: Theme.of(context).colorScheme.primary)),
+                        width: 40.h,
+                        height: 40.h,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "1",
+                          ),
+                        ),
+                      ),
+                    )),
+                Icon(
+                  Icons.arrow_forward,
+                  size: 20.h,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.h),
+                  child: GestureDetector(
+                    child: Container(
+                      child: Text("2"),
+                    ),
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsets.all(10.h),
+                    child: GestureDetector(
+                      child: GestureDetector(
+                        child: Container(
+                          child: Text("3"),
+                        ),
+                      ),
+                    )),
+                Padding(
+                    padding: EdgeInsets.all(10.h),
+                    child: GestureDetector(
+                      child: GestureDetector(
+                        child: Container(
+                          child: Text("4"),
+                        ),
+                      ),
+                    ))
+              ],
+            );
+          })
+        ]),
+
         persistentFooterButtons: [getFloatingButtons()],
         // bottomNavigationBar: getFloatingButtons(),
         // floatingActionButton: ,
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         body: PageView(
             onPageChanged: ((value) {
-              notifier.currentPage = value;
+              switchPagesNotifier.currentPage = value;
             }),
             controller: _pageController,
             children: [FirstAddPage(inputNotifier), SecondAddPage()]),
@@ -51,27 +115,23 @@ class Add extends StatelessWidget {
   }
 
   Widget getFloatingButtons() {
-    List<Widget> rowChildren = [];
-    try {
-      print(_pageController.page);
-    } on AssertionError catch (e) {
-      print(_pageController.initialPage);
-    }
-
     return Consumer<AddPageNotifier>(builder: (context, model, child) {
       return Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           model.currentPage == firstPage
               ? const SizedBox.shrink()
-              : getNavigationButton(Icons.arrow_back, previousPage),
-          getNavigationButton(Icons.arrow_forward, nextPage),
+              : getNavigationButton(Icons.arrow_back, previousPage, context),
+          getNavigationButton(Icons.arrow_forward, nextPage, context),
         ],
       );
     });
   }
 
-  Widget getNavigationButton(IconData icon, void Function() onPressedFunction) {
+  Widget getNavigationButton(
+      IconData icon,
+      void Function(BuildContext context) onPressedFunction,
+      BuildContext context) {
     return SizedBox(
         width: 100,
         child: ElevatedButton(
@@ -84,18 +144,22 @@ class Add extends StatelessWidget {
           ),
           // splashColor: Colors.transparent,
 
-          onPressed: () => onPressedFunction(),
+          onPressed: () => onPressedFunction(context),
           child: Icon(icon),
         ));
   }
 
-  void nextPage() {
+  void nextPage(BuildContext context) {
     _pageController.nextPage(
         duration: animationDuration, curve: animationCurve);
+    Provider.of<AddPageNotifier>(context, listen: false).currentPage =
+        _pageController.page as int;
   }
 
-  void previousPage() {
+  void previousPage(BuildContext context) {
     _pageController.previousPage(
         duration: animationDuration, curve: animationCurve);
+    Provider.of<AddPageNotifier>(context, listen: false).currentPage =
+        _pageController.page as int;
   }
 }
