fabe51d91081bece503044008c3549f390e30f7a
Mattis Kunstmann <mattis.kunstmann@outlook.de>
2023-01-05 13:36:16 +0100

    in progress

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -6,7 +6,7 @@ import '../utils/helpers/builders.dart';
 
 class OthiaConstants {
   static final String awsApiEndpoint = getAwsApiEndpoint();
-  static const String othiaDomain = 'othia.de';
+  static const String othiaDomain = 'www.othia.de';
 
   //TODO make this constant together with the link in rest api services
   static const String eventDetailPath = 'events';
