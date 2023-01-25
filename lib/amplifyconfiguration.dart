d786a8acf1a81b4a85daccbb924ebcf75601248f
Mattis Kunstmann <mattis.kunstmann@outlook.de>
2023-01-25 21:54:14 +0100

    mostly finished login and signup

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,66 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "othiaApi": {
+                    "endpointType": "REST",
+                    "endpoint": "https://296g17wt2h.execute-api.eu-central-1.amazonaws.com/dev",
+                    "region": "eu-central-1",
+                    "authorizationType": "AWS_IAM"
+                }
+            }
+        }
+    },
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "eu-central-1:6170e889-7213-47a2-8f42-ac34b681a9de",
+                            "Region": "eu-central-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "eu-central-1_KAuZBP3aV",
+                        "AppClientId": "2p3s6gslqb57o0aij8egdi4eir",
+                        "Region": "eu-central-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "socialProviders": [],
+                        "usernameAttributes": [
+                            "EMAIL"
+                        ],
+                        "signupAttributes": [
+                            "EMAIL"
+                        ],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": []
+                        },
+                        "mfaConfiguration": "OPTIONAL",
+                        "mfaTypes": [
+                            "SMS",
+                            "TOTP"
+                        ],
+                        "verificationMechanisms": [
+                            "EMAIL"
+                        ]
+                    }
+                }
+            }
+        }
+    }
+}''';
