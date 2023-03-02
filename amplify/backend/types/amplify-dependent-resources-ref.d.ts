48fcccab2ff91a4d2ddd3239a08a45a60b129a7c
Hannes Kunstmann <h.kunstmann@outlook.com>
2023-03-02 14:05:13 +0100

    tracking user actions works for not-logged in users

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "OthiaAuth": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "analytics": {
+        "OthiaPinpoint": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    }
+}
